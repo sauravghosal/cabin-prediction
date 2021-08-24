6d874c79be152dfe6bc80e95ac2b6cf1cd93419f
Saurav Ghosal <sghosal9@gatech.edu>
2021-08-24 12:33:05 -0400

    occupancy data returned in date order

--- ec2/server/app.py
+++ ec2/server/app.py
@@ -5,18 +5,21 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 from pathlib import Path
 import logging
-import requests
+import re
+import atexit
+import pandas
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 logging.basicConfig(filename=Path(__file__).parent.joinpath(
     'debug.log'), level=logging.DEBUG)
-load_dotenv(dotenv_path=(Path(__file__).parents[1]).joinpath('.env'))
+load_dotenv(dotenv_path='.env')
 
 db_host = os.getenv('DB_DATABASE')
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
-db_name = "Cabins"
+db_name = "cabins"
 
 
 engine = create_engine(
@@ -31,7 +34,7 @@ def query_occupancy_by_date_range(table_name, start_date, end_date):
         app.logger.info('Finding occupancy data between %s and %s for cabin %s',
                         start_date, end_date, table_name)
         return conn.execute(f"""
-          SELECT * FROM `{table_name}` WHERE date BETWEEN '{start_date:%Y-%m-%d}' AND '{end_date:%Y-%m-%d}'
+          SELECT * FROM `{table_name}` WHERE date BETWEEN '{start_date:%Y-%m-%d}' AND '{end_date:%Y-%m-%d} ORDER BY date ASC'
           """)
     except (Exception) as error:
         app.logger.error(error)
@@ -43,7 +46,7 @@ def query_occupancy_by_date_range(table_name, start_date, end_date):
 def query_all_cabins():
     try:
         conn = engine.connect()
-        return conn.execute(f"""select table_name from INFORMATION_SCHEMA.TABLES where TABLE_TYPE='BASE TABLE' AND table_schema='Cabins'""")
+        return conn.execute(f"""select table_name from INFORMATION_SCHEMA.TABLES where TABLE_TYPE='BASE TABLE' AND table_schema='cabins'""")
     except (Exception) as error:
         app.logger.error(error)
     finally:
@@ -51,28 +54,20 @@ def query_all_cabins():
         engine.dispose()
 
 # home page
-
-
 @app.route('/', methods=['GET'])
 def home():
-    response = requests.get('http://18.117.140.76/api',
-                            params={'start_date': '10-10-2019', 'end_date': '11-10-2019', 'cabin': 'Smoky Mountain Lodge'})
     return render_template('index.html')
-
-# TODO: create REST API endpoint to list available cabins
 
 
 @app.route('/cabins', methods=['GET'])
 def list_all_cabins():
     cabins = []
     for cabin in query_all_cabins():
-        cabins.append(cabin)
+        cabins.append(re.search('([\'\"](.*)[\'\"])', str(cabin)).group(1))
     return dict(cabin=cabins)
 
 # TODO: query string param validation on start_date, end_date, and cabin
-
-
-@app.route('/api/occupancy', methods=["GET"])
+@app.route('/occupancy', methods=["GET"])
 def query_by_date():
     start_date, end_date, cabin_name = request.args.get(
         "start_date"), request.args.get("end_date"), request.args.get("cabin")
@@ -84,12 +79,15 @@ def query_by_date():
         table_name=cabin_name, start_date=start_date, end_date=end_date)
     occupancy_data = {}
     for row in rows:
-        app.logger.debug(row[0])
+        print(row)
         if row[1]:
             occupancy_data.update(
-                {row[0].strftime('%m-%d-%Y'): row[1].decode('utf-8')})
+                {row[0].strftime('%m-%d-%Y'): row[1]})
     return dict(occupancy=occupancy_data)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# def close_db_connection():
+#     app.logger.info('Closing db connection')
+#     engine.dispose()
+
+# atexit.register(close_db_connection)
