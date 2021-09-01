5dc48db158b733e1dfa2483751d5187367def2b9
Saurav Ghosal <sghosal9@gatech.edu>
2021-09-01 13:54:10 -0400

    removing ant design ui from front end

--- ec2/client/src/main.js
+++ ec2/client/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "ant-design-vue/dist/antd.css";
 import BalmUI from "balm-ui"; // Official Google Material Components
 import BalmUIPlus from "balm-ui-plus"; // BalmJS Team Material Components
 import "balm-ui-css";
