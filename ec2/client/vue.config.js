bd09f3aec5d123e40863efb8700d99e92b114308
Saurav Ghosal <sghosal9@gatech.edu>
2021-09-02 02:58:01 -0400

    removing balm ui from vue config

--- ec2/client/vue.config.js
+++ ec2/client/vue.config.js
@@ -7,14 +7,5 @@ module.exports = {
   outputDir:
     process.env.NODE_ENV == "development" ? "dist" : "../server/templates",
   assetsDir: process.env.NODE_ENV == "development" ? "" : "../static",
-  runtimeCompiler: true,
   // NOTE: set alias via `configureWebpack` or `chainWebpack`
-  configureWebpack: {
-    resolve: {
-      alias: {
-        "balm-ui-plus": "balm-ui/dist/balm-ui-plus.js",
-        "balm-ui-css": "balm-ui/dist/balm-ui.css",
-      },
-    },
-  },
 };
