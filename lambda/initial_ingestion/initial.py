3d37881a64e4e29f15b59cdb08042dbea1284ed2
Saurav Ghosal <sghosal9@gatech.edu>
2021-09-05 16:35:53 -0400

    initial ingestion works and handler file rename

--- lambda/initial_ingestion/initial.py
+++ lambda/initial_ingestion/initial.py
@@ -1,23 +1,26 @@
 from datetime import date
-from random import randint
 import pandas as pd
-import logging
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, DATETIME, NVARCHAR
+from sqlalchemy.sql.sqltypes import SMALLINT
+from appsupport import updateCabinColumns
+import atexit
+import boto3
+import io
+import json
 
 # change to aws lambda environment variables
 load_dotenv()
 
 
-logging.basicConfig(
-    level=logging.DEBUG)
-
-db_host = os.getenv('DB_DATABASE')
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
+db_host = os.environ['DB_DATABASE']
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASSWORD']
 db_name = "cabins"
 
+
+s3 = boto3.client('s3')
 
 # TODO: clean up SQL queries with Session/Transaction ORM from SQLAlchemy?
 # https://docs.sqlalchemy.org/en/14/core/tutorial.html
@@ -83,17 +86,42 @@ def ingest_data(bits, table_name):
         raise error
     finally:
         conn.close()
-
-
-def handler(event, context):
-    xls = pd.ExcelFile('output.xlsx')
-    for cabin in xls.sheet_names[1:]:
-        df = xls.parse(sheet_name=cabin)
-        df['Date'] = pd.to_datetime(
-            arg=df['Date'], format="%Y_%m_%d", errors='ignore')
-        df.drop(df.columns[[1, 2, 3, 4, 5, 6, 7]], axis=1, inplace=True)
-        print(df)
-        df.to_sql(cabin.strip(), engine, index=False,
-                  schema=db_name, if_exists="replace", dtype={'date': DATETIME, 'occupancy': NVARCHAR(180)})
-    engine.dispose()
+        
+def download_output_file(bucket_name, folder_prefix):
+    scrape_files = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_prefix)['Contents']
+    latest_output_file = max(scrape_files, key=lambda x: x['LastModified'])
+    io_stream = io.BytesIO()
+    s3.download_fileobj(bucket_name, latest_output_file['Key'], io_stream)
+    io_stream.seek(0)
+    return io_stream
     
+    
+def handler(event, context):
+    try: 
+        file_contents = download_output_file(bucket_name="ghosalre", folder_prefix='scrape output')
+        xls = pd.ExcelFile(file_contents)
+        for cabin in xls.sheet_names[1:]:
+            df = xls.parse(sheet_name=cabin)
+            df['date'] = pd.to_datetime(
+                arg=df['Date'], format="%Y_%m_%d", errors='ignore')
+            df.drop(df.columns[[1, 2, 3, 4, 5, 6, 7]], axis=1, inplace=True)
+            updateCabinColumns(cabin_name=cabin, cabin_df=df)
+            df.drop(['Date', 'dow', 'C', 'B', 'tot_count', 'occ_delta'], axis=1, inplace=True)
+            print("Ingesting cabin {} into database".format(cabin))
+            # df.to_sql(cabin.strip(), engine, index=True,
+            #         schema=db_name, if_exists="replace", dtype={'date': DATETIME, 'occupancy': NVARCHAR(180), 'occ_count': SMALLINT, 'bmap_diff': NVARCHAR(180)})
+            return {
+                "statusCode": 200,
+                "body": json.dumps({
+                "message": "ingestion complete!",
+                })
+            }
+    except Exception as err:
+        raise err
+    
+   
+def close_db_connection():
+    print('Closing db connection')
+    engine.dispose()
+
+atexit.register(close_db_connection)
