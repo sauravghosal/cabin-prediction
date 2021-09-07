fbd2b3f764c1acd332a3e079884c7f9067c43a71
Saurav Ghosal <sghosal9@gatech.edu>
2021-09-06 20:05:18 -0400

    scrape file parsing complete

--- lambda/incremental_ingestion/incremental.py
+++ lambda/incremental_ingestion/incremental.py
@@ -1,12 +1,7 @@
-import shutil
-import os, platform
+import os
 from glob import glob
 import datetime
-import inspect
 import pandas as pd
-from sauravsupport import getCabinNumbers
-import cabinsofinterest
-import logging, sys
 from pathlib import Path
 from cabinsofinterest import CabinsOfInterest
 from appsupport import fixAllOccupancyBitmaps, fixOccupancyBitmap
@@ -32,44 +27,7 @@ engine = create_engine(
     f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}')
 
 
-def get_most_recent_date(table_name):
-    command = (f"""SELECT MAX (Date) AS \"Max Date\" FROM `{table_name}`""")
-    try:
-        conn = engine.connect()
-        conn.execute(command)
-        # commit the changes
-    except (Exception) as error:
-        raise error
-    finally:
-        conn.close()
-
-def download_new_scrape_files(bucket_name, folder_prefix, date):
-    print('Downloading new scrape files from {}'.format(bucket_name))
-    scrape_files = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_prefix)['Contents']
-    latest_scrape_files = list(filter(lambda x: IsValidFile(x['Key'].split('/')[-1], date), scrape_files))
-    print('Found {} new scrape files'.format(len(latest_scrape_files)))
-    for scrape_file in latest_scrape_files:
-        io_stream = io.BytesIO()
-        s3.download_fileobj(bucket_name, scrape_file['Key'], io_stream)
-        io_stream.seek(0)
-        yield io_stream
-
-def IsValidFile(fn, prev_date):
-
-    # Check for 2nd half of file name
-    if (fn[10:].split('.')[0] != '_Data_Scraping_Output'):
-        #print("Invalid file name: {}".format(fn))
-        return False
-    #Check for date part of the file name
-    try:
-        date = datetime.datetime.strptime(fn[0:10], '%Y_%m_%d')
-        if date > prev_date:
-            return True
-        return False
-    except ValueError as err:
-        print("Invalid date in file name: {}".format(err))
-        return False
-    
+# SQL QUERIES
 def get_most_recent_date(table_name):
     try:
         conn = engine.connect()
@@ -91,27 +49,53 @@ def query_all_cabins():
         raise error
     finally:
         conn.close()
+        
+def ingest_data(bits: str, date: datetime.datetime, table_name: str):
+    try:
+        conn = engine.connect()
+        print('Ingesting occupancy data from date {:%Y-%m-%d} for cabin {}'.format(date, table_name))
+        conn.execute(
+            f"""INSERT INTO `{table_name.strip()}` (date, occupancy) VALUES ('{date:%Y-%m-%d})', '{bits}')""")
+    except (Exception) as error:
+        print(error)
+        raise error
+    finally:
+        conn.close()
+        
+# S3 QUERIES
+def download_new_scrape_files(bucket_name, folder_prefix, date):
+    print('Downloading new scrape files from {}'.format(bucket_name))
+    scrape_files = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_prefix)['Contents']
+    latest_scrape_files = list(filter(lambda x: IsValidFile(x['Key'].split('/')[-1], date), scrape_files))
+    print('Found {} new scrape files'.format(len(latest_scrape_files)))
+    for scrape_file in latest_scrape_files:
+        io_stream = io.BytesIO()
+        s3.download_fileobj(bucket_name, scrape_file['Key'], io_stream)
+        io_stream.seek(0)
+        yield (scrape_file['Key'].split('/')[-1], io_stream)
+        
 
 
-def update_output_file(file):
-    #################################################################
-    #   Reading 'output.xlsx' file
-    #################################################################
+def IsValidFile(fn, prev_date):
+    # Check for 2nd half of file name
+    if (fn[10:].split('.')[0] != '_Data_Scraping_Output'):
+        #print("Invalid file name: {}".format(fn))
+        return False
+    #Check for date part of the file name
+    try:
+        date = datetime.datetime.strptime(fn[0:10], '%Y_%m_%d')
+        if date > prev_date:
+            return True
+        return False
+    except ValueError as err:
+        print("Invalid date in file name: {}".format(err))
+        return False
     
 
-    #################################
-    #   If no 'Occupancy' row
-    #   then create this row and save it HERE
-    #################################
-
-    #
-    #
-    #
-
-
-    ##############################################################
-    #   Reading new input files ... and appending data to old file
-    scrapeWB = pd.read_excel(file, sheet_name=None,
+# SCRAPE FILE PARSING AND INGESTION
+def update_output_file(fn, file_data):
+    # TODO: fix all occupancy bit maps -> not sure how much prior data is needed
+    scrapeWB = pd.read_excel(file_data, sheet_name=None,
                             dtype={'monthAvailabe_1': str,
                                 'monthAvailabe_2': str,
                                 'monthAvailabe_3': str,
@@ -120,68 +104,36 @@ def update_output_file(file):
                                 'monthAvailabe_6': str,
                                 'monthAvailabe_7': str
                                 })
-    #         #### HACK! HACK ! HACK!
+    #### HACK! HACK ! HACK!
     #### For cabinsusa, remove " Cabin Rental" from cabinname
     if ('cabinsusa' in scrapeWB.keys()):
         scrapeWB['cabinsusa']['CabinName'] = scrapeWB['cabinsusa']\
             ['CabinName'].str.replace(" Cabin Rental", "")
-    print(scrapeWB)
     #### end of hack ####
-    #         myseries = pd.Series(getCabinNumbers(scrapeWB), name=fn[0:10])
-    #         try:
-    #             cc_dict['__number_cabins'] = cc_dict.setdefault('__number_cabins', pd.DataFrame(columns=[])).append(myseries, verify_integrity=True)
-    #         except ValueError as err:
-    #             print('__number_cabins overlapping index {} .. not inserting'.
-    #                   format(fn[0:10]))
-    #         print('Reading {}'.format(fn[0:10]))
-    #         for mgmtco in scrapeWB.keys():
-    #             if (mgmtco in CabinsOfInterest.keys()):
-    #                 scrapeWB[mgmtco].set_index('CabinName', inplace=True)
-    #                 scrapeWB[mgmtco].columns = \
-    #                     [x.replace('monthAvailabe_','month') if ('monthAvailabe_' in x) else x for x in scrapeWB[mgmtco].columns]
-    #                 cabinList = CabinsOfInterest[mgmtco]
-    #                 for cabin in cabinList:
-    #                     #print('1. Cabin being checked .... ', cabin)
-    #                     try:
-    #                         cabin_info = scrapeWB[mgmtco].loc[cabin,'month1':'month7'].str.replace('\'','')
-    #                         #
-    #                         #   Calculate and add the 'occupancy'
-    #                         #
-    #                         #
-    #                         #print('2. Cabin being checked .... ', cabin)
-    #                         cabin_info.name = fn[0:10]
-    #                         cabin_info['occupancy'] = (fixOccupancyBitmap(cabin, cabin_info.name,
-    #                                            [cabin_info['month1'],
-    #                                             cabin_info['month2'],
-    #                                             cabin_info['month3'],
-    #                                             cabin_info['month4'],
-    #                                             cabin_info['month5'],
-    #                                             cabin_info['month6'],
-    #                                             cabin_info['month7']]))
-    #                         cc_dict[cabin] = cc_dict.setdefault(cabin, pd.DataFrame(columns=[])).append(cabin_info, verify_integrity=True)
-    #                         print('\t{} {}'.format(cabin, mgmtco))
-    #                         dirtyFlag = True
-    #                     except KeyError:
-    #                         print ("KeyError .. could not add cabin .. {} {}".format(cabin, fn[0:10]))
-    #                         pass
-    #                     except ValueError as err:
-    #                         print('\t***{} could not add {} due to overlap'.format(cabin, fn[0:10]))
+    for mgmtco in scrapeWB.keys():
+        if (mgmtco in CabinsOfInterest.keys()):
+            scrapeWB[mgmtco].set_index('CabinName', inplace=True)
+            scrapeWB[mgmtco].columns = \
+                [x.replace('monthAvailabe_','month') if ('monthAvailabe_' in x) else x for x in scrapeWB[mgmtco].columns]
+            cabinList = CabinsOfInterest[mgmtco]
+            for cabin in cabinList:
+                try:
+                    cabin_info = scrapeWB[mgmtco].loc[cabin,'month1':'month7'].str.replace('\'','')              
+                    cabin_info['occupancy'] = (fixOccupancyBitmap(cabin, fn[0:10],
+                                        [cabin_info['month1'],
+                                        cabin_info['month2'],
+                                        cabin_info['month3'],
+                                        cabin_info['month4'],
+                                        cabin_info['month5'],
+                                        cabin_info['month6'],
+                                        cabin_info['month7']]))
+                    ingest_data(cabin_info['occupancy'], datetime.datetime.strptime(fn[0:10], '%Y_%m_%d'), cabin)
+                except KeyError:
+                    print ("KeyError .. could not add cabin .. {} {}".format(cabin, fn[0:10]))
+                    pass
+                except ValueError as err:
+                    print('\t***{} could not add {} due to overlap'.format(cabin, fn[0:10]))
     
-    # TODO: ingest into database
-    
-    # if (not dirtyFlag):
-    #     print("No new entries, lastDate={} .. skipping write".format(lastDate))
-    # else:
-    #     with pd.ExcelWriter(WriteTestFileName) as writer:
-    #         print('Opening to write {}'.format(WriteTestFileName))
-    #         for cabinName, data in cc_dict.items():
-    #             print('\tWriting {:25}{:4} lines'.
-    #                   format(cabinName, len(data.index)))
-    #             data = data.astype(str)
-    #             data.to_excel(writer, sheet_name=cabinName, index_label='Date')
-    #         cc_dict['__number_cabins'].to_excel(writer,
-    #                         sheet_name='__number_cabins', index_label='Date')
-            
         
 
 
@@ -191,14 +143,13 @@ def handler(event, context):
     for cabin in cabins[2:]:
         if get_most_recent_date(cabin) > latest_date:
             latest_date = get_most_recent_date(cabin) 
-    print(latest_date)
-    for scrape_file in download_new_scrape_files(bucket_name="ghosalre", folder_prefix='Data', date=latest_date):
-        update_output_file(scrape_file)
+    print(f"Last date database was updated: {latest_date:%Y_%m_%d}")
+    for (fn, file_data) in download_new_scrape_files(bucket_name="ghosalre", folder_prefix='Data', date=latest_date):
+        update_output_file(fn, file_data)
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "incremental ingestion complete!",
-            # "location": ip.text.replace("\n", "")
         }),
     }
 
